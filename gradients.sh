dee39b247be89749250cf12093d252a63d8dfe9e
Bas Nelissen <b.g.l.nelissen@gmail.com>
2014-09-09 16:44:24 +0200

    Code cleanup

--- gradients.sh
+++ gradients.sh
@@ -1,46 +1,45 @@
 #!/bin/bash
-# create gradients from json source
+# Create beautiful png background gradients
+# Use the json source from https://github.com/Ghosh/uiGradients
 
-# depends on: convert parallel jq
+# depends on: convert jq parallel
 # brew install imagemagick parallel jq
 
+
+# get colours and names from the source
+# convert the json to a bash readable input (#348F50 #56B4D3 Emerald Water)
+# loop through each gradient and use convert (ImageMagick) to create a PNG
+# Rotate -90 to create a nice horizontal gradient
+
+# use as:
+# ./gradients.sh | parallel
+
 # variables
-OUTPUTDIR="./Output-Gradients" # output directory # .gitignore this
+OUTPUTDIR="./Output-Gradients" # output directory, .gitignore this
 WIDTH="2560"   # rMBP-13
 HEIGHT="1600"
-iPhone5OUTPUTDIR="./Output-Gradients-iPhone5" # output directory # .gitignore this
-iPhone5WIDTH="640" # iPhone5
-iPhone5HEIGHT="1136"
-iPhone4OUTPUTDIR="./Output-Gradients-iPhone4" # output directory # .gitignore this
-iPhone4WIDTH="640" # iPhone4
-iPhone4HEIGHT="960"
+# OUTPUTDIR="./Output-Gradients" # iPhone 5
+# WIDTH="640" 
+# HEIGHT="1136"
+# OUTPUTDIR="./Output-Gradients" # iPhone 4
+# WIDTH="640" 
+# HEIGHT="960"
 
-# get colours and names, clean'm, loop'm, echo'm
+# fetch json from repo and loop through gradients
 while read -r line; do
-  # create variables
-  GRADIENT="$(echo "$line" | awk '{print $1 "-" $2}')"
-  NAME="$(echo "$line" | cut -d " " -f 3-)"
-  # vertical
-  # mkdir -p "$OUTPUTDIR" && echo "convert -size "${WIDTH}x${HEIGHT}" gradient:\"$GRADIENT\" "$OUTPUTDIR"/\""${NAME}"-vertical.png\""
-  # horizontal
-  mkdir -p "$OUTPUTDIR" && echo "convert -size "${HEIGHT}x${WIDTH}" gradient:\"$GRADIENT\" -rotate -90 "$OUTPUTDIR"/\""${NAME}"-horizontal.png\""
-  # iPhone 5
-  FILENAME="$iPhone5OUTPUTDIR""/""${NAME}"".png"
-  if [ -f "$FILENAME" ]; then
-    echo "echo \"Already exists: $FILENAME\""
+  if [ -n "$line" ]; then
+    # create variables
+    GRADIENT="$(echo "$line" | awk '{print $1 "-" $2}')"
+    NAME="$(echo "$line" | cut -d " " -f 3-)"
+    # Let's roll
+    FILENAME="$OUTPUTDIR""/""${NAME}"".png"
+    if [ ! -f "$FILENAME" ]; then
+      echo "mkdir -p \"$OUTPUTDIR\" && convert -size \"${HEIGHT}x${WIDTH}\" -rotate 90 gradient:\"$GRADIENT\" \"$FILENAME\" "
+    fi
   else
-    echo "echo \"Creating: $FILENAME\""
-    echo "mkdir -p \"$iPhone5OUTPUTDIR\" && convert -size \"${iPhone5WIDTH}x${iPhone5HEIGHT}\" gradient:\"$GRADIENT\" \"$FILENAME\" "
-  fi
-  # iPhone 4
-  FILENAME="$iPhone4OUTPUTDIR""/""${NAME}"".png"
-  if [ -f "$FILENAME" ]; then
-    echo "echo \"Already exists: $FILENAME\""
-  else
-    echo "echo \"Creating: $FILENAME\""
-    echo "mkdir -p \"$iPhone5OUTPUTDIR\" && convert -size \"${iPhone5WIDTH}x${iPhone5HEIGHT}\" gradient:\"$GRADIENT\" \"$FILENAME\" "
+    echo "No valid input"
+    exit 1
   fi
 done <<< "$(curl https://raw.githubusercontent.com/Ghosh/uiGradients/master/gradients.json | \
             jq '.[] | .colour1 + " " + .colour2 + " " + .name' | \
-            sed 's/\"//g')" | \
-            parallel
+            sed 's/\"//g')"
