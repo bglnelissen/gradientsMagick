cac7ccd6e85ae58692cd5638dd6fbbc20feac4f8
Bas Nelissen <b.g.l.nelissen@gmail.com>
2014-09-30 12:47:27 +0200

    edit conflict

--- gradients.sh
+++ gradients.sh
@@ -18,14 +18,10 @@
 OUTPUTDIR="./Output-Gradients" # output directory, .gitignore this
 WIDTH="2560"   # rMBP-13
 HEIGHT="1600"
-# OUTPUTDIR="./Output-Gradients" # iPhone 5
 OPTIONS="-size \"${HEIGHT}x${WIDTH}\"  -rotate 90"
 # OUTPUTDIR="./Output-Gradients-iPhone5" # iPhone 5
 # WIDTH="640" 
 # HEIGHT="1136"
-# OUTPUTDIR="./Output-Gradients" # iPhone 4
-# WIDTH="640" 
-# HEIGHT="960"
 # OPTIONS="-size \"${WIDTH}x${HEIGHT}\" "
 
 mkdir -p "$OUTPUTDIR"
@@ -38,7 +34,6 @@ while read -r line; do
     # Let's roll
     FILENAME="$OUTPUTDIR""/""${NAME}"".png"
     if [ ! -f "$FILENAME" ]; then
-      echo "mkdir -p \"$OUTPUTDIR\" && convert -size \"${HEIGHT}x${WIDTH}\" -rotate 90 gradient:\"$GRADIENT\" \"$FILENAME\" "
       echo "mkdir -p \"$OUTPUTDIR\" && convert $OPTIONS gradient:\"$GRADIENT\" \"$FILENAME\" "
     fi
   else
