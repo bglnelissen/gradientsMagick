8fc611c25523c0ab92df5bbfe6618a88b98d8dd3
Bas Nelissen <bas@koekoekmbp.home>
2014-09-30 12:48:46 +0200

    conflict fix

--- gradients.sh
+++ gradients.sh
@@ -18,12 +18,15 @@
 OUTPUTDIR="./Output-Gradients" # output directory, .gitignore this
 WIDTH="2560"   # rMBP-13
 HEIGHT="1600"
+OPTIONS="-size \"${HEIGHT}x${WIDTH}\" -rotate 90 "
 OUTPUTDIR="./Output-Gradients-iPhone5" # iPhone 5
 WIDTH="640" 
 HEIGHT="1136"
+OPTIONS="-size \"${WIDTH}x${HEIGHT}\" "
 # OUTPUTDIR="./Output-Gradients" # iPhone 4
 # WIDTH="640" 
 # HEIGHT="960"
+OPTIONS="-size \"${WIDTH}x${HEIGHT}\" "
 
 # fetch json from repo and loop through gradients
 while read -r line; do
@@ -34,7 +37,7 @@ while read -r line; do
     # Let's roll
     FILENAME="$OUTPUTDIR""/""${NAME}"".png"
     if [ ! -f "$FILENAME" ]; then
-      echo "mkdir -p \"$OUTPUTDIR\" && convert -size \"${HEIGHT}x${WIDTH}\" -rotate 90 gradient:\"$GRADIENT\" \"$FILENAME\" "
+      echo "mkdir -p \"$OUTPUTDIR\" && convert "$OPTIONS" gradient:\"$GRADIENT\" \"$FILENAME\" "
     fi
   else
     echo "No valid input"
